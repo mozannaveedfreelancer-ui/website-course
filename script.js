6601cd5fb6602c970033ab4ed36b5abc7c4f7bce
Rana Mozan <mozannaveedfreelance@gmail.com>
2026-07-15 10:18:17 +0500

    Added Email Appointment System

--- script.js
+++ script.js
@@ -7,14 +7,30 @@ form.addEventListener("submit", function(event){
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let phone = document.getElementById("phone").value;
-   
+    let date = document.getElementById("date").value;
+let department = document.getElementById("department").value;
 
-    if(name==="" || email==="" || phone===""){
+    if(name === "" || email === "" || phone === ""){
         alert("Please fill all fields.");
     }
     else{
-        alert("Appointment Booked Successfully!");
-        form.reset();
+
+      emailjs.send("service_d1z53t1", "template_zxcu7me", {
+    name: name,
+    email: email,
+    phone: phone,
+    date: date,
+    department: department
+})
+        .then(function () {
+            alert("Appointment Booked Successfully!");
+            form.reset();
+        })
+        .catch(function (error) {
+            alert("Email Failed!");
+            console.log(error);
+        });
+
     }
 
 });
